aed3856f26dc36ac7ec619829af65f1b93aa32d9
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-08-26 11:15:13 -0400

    feat(useEffect, Feed, hooks): add hooks and useEffect for fetching data, import Feed component, and pass props to Header and Feed component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,48 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../Header/Header";
+import Feed from "../Feed/Feed";
 function App() {
-  const [searchChange, setSearchChange] = useState("");
+  const [filter, setFilter] = useState("hot");
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
   const handleSearchChange = (e) => {
-    setSearchChange(e.target.value);
+    setQuery(e.target.value);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response;
+      // If query is not empty, fetch data from the search endpoint using query and the filter
+      if (query) {
+        // Search endpoint
+        response = await fetch(
+          `https://api.reddit.com/search.json?q=${query}&sort=${filter}`
+        );
+      } else {
+        //Listings endpoint
+        response = await fetch(`https://api.reddit.com/${filter}.json`);
+      }
+      const results = await response.json();
+      console.log("results:", results);
+      setResults(results);
+    };
+    fetchData();
+  }, [query, filter]);
+
+  console.log(query);
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Header handleChange={handleSearchChange} value={searchChange} />
+          <Header
+            searchValue={query}
+            handleSearchChange={handleSearchChange}
+            setFilter={setFilter}
+          />
+          <Feed results={results} onFilterChange={setFilter} />
         </div>
       </header>
     </div>
