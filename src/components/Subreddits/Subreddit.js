f5a1f3298b1382b23dd6a4c70363833bebde0a02
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-08-29 10:36:23 -0400

    feat(Subreddit): create and export Subreddit component

---
+++ src/components/Subreddits/Subreddit.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import "./Subreddit.css";
+function SubredditList() {
+  const [subredditList, setSubredditList] = useState([]);
+
+  useEffect(() => {
+    const fetchSubreddits = async () => {
+      try {
+        const response = await fetch("https://www.reddit.com/subreddits.json");
+        const data = await response.json();
+        setSubredditList(data.data.children);
+      } catch (error) {
+        console.log(`Error fetching subreddits: ${error}`);
+      }
+    };
+
+    fetchSubreddits();
+  }, []);
+
+  return (
+    <div className="subreddit-list">
+      {/* <span className="subreddit-title">Subreddits: </span> */}
+      {subredditList.map((subreddit) => (
+        <p key={subreddit.data.id} className="subreddit-button">
+          r/{subreddit.data.display_name}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default SubredditList;
