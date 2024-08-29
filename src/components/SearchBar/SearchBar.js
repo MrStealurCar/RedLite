676fb25df631633e53d24fc0a3c5424f19cb00aa
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-08-29 10:35:32 -0400

    perf(SearchBar): destructure props in SearchBar component and pass them to input element

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,17 @@
 import "./SearchBar.css";
 import searchIcon from "./search-icon.png";
 
-function SearchBar({ handleSearchChange, searchChange }) {
+function SearchBar({ handleSearchChange, searchValue }) {
   return (
     <div className="searchContainer">
       <input
         placeholder="Search Subreddits"
         className="searchBar"
-        onChange={handleSearchChange}
-        value={searchChange}
+        handleSearchChange={handleSearchChange}
+        searchValue={searchValue}
       />
       <img src={searchIcon} alt="Search icon" className="searchIcon" />
     </div>
   );
 }
-
 export default SearchBar;
