c616c46839feddb61ba2ac0c099b41ccc8b356fd
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-09-11 14:01:59 -0400

    refactor(Header): fix classNames to be separated by - instead of camelCasing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 import SearchBar from "../SearchBar/SearchBar";
-function Header({ searchValue, handleSearchChange, setFilter }) {
+function Header({ searchValue, handleSearchChange, setFilter, searchResults }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
+
   return (
     <header>
-      <div className="headContainer">
-        <div className="headInfo">
+      <div className="head-container">
+        <div className="head-info">
           <img src="favicon.png" alt="redlite log" className="logo" />
-          <span>
+          <span onClick={handleClick} className="title">
             red<span className="highlight">lite</span>
           </span>
         </div>
-        <div className="searchWrapper">
+        <div className="search-wrapper">
           <SearchBar
+            searchResults={searchResults}
             searchValue={searchValue}
-            onChange={handleSearchChange}
+            handleSearchChange={handleSearchChange}
             setFilter={setFilter}
           />
         </div>
