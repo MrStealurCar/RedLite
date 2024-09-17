a02580a030d180b78b8035fa2209e540b9dc0cab
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-09-17 10:14:51 -0400

    feat(VoteButtons): add function to reduce score number

--- src/components/VoteButtons/VoteButtons.js
+++ src/components/VoteButtons/VoteButtons.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import "./VoteButtons.css";
-function VoteButtons() {
-  const [vote, setVote] = useState(null);
 
+function VoteButtons({ score }) {
+  const [vote, setVote] = useState(null);
   const handleVote = (type, event) => {
     event.stopPropagation();
     setVote(type);
   };
+
+  function formatNumber(num) {
+    if (!num) return 0;
+
+    if (num >= 1e6) {
+      return (num / 1e6).toFixed(1) + "M"; // Converts to millions and adds 'M'
+    } else if (num >= 1e3) {
+      return (num / 1e3).toFixed(1) + "k"; // Converts to thousands and adds 'k'
+    } else {
+      return num.toString(); // Returns the number as is if less than 1,000
+    }
+  }
+
+  const formattedNumber = formatNumber(score);
+
   return (
     <div className="vote-buttons">
       <i
@@ -15,6 +30,17 @@ function VoteButtons() {
           vote === "upvote" ? "like" : "default-upvote"
         }`}
       ></i>
+      <span
+        className={`post-score ${
+          vote === "upvote"
+            ? "score-upvoted"
+            : vote === "downvote"
+            ? "score-downvoted"
+            : ""
+        }`}
+      >
+        {formattedNumber}
+      </span>
       <i
         onClick={(e) => handleVote("downvote", e)}
         className={`fa fa-arrow-down ${
