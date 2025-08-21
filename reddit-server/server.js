25840ce3aab898bc89a6d24a07745086e1eeabd4
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-08-21 12:52:42 -0400

    feat(server): import and register routes for each express file

--- reddit-server/server.js
+++ reddit-server/server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
-
+import feedRouter from "./routes/feed";
+import postRouter from "./routes/posts";
+import commentsRouter from "./routes/comments";
 app.use(express.json());
 app.use(cors());
+// Route registers
+app.use("/feed", feedRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentsRouter);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
