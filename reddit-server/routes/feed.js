ae04216f53e7e13d0aa70d72f9a59c918807a603
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-08-22 12:47:44 -0400

    feat(feed): add main feed and subreddit get routes

--- reddit-server/routes/feed.js
+++ reddit-server/routes/feed.js
@@ -1,4 +1,35 @@
 const express = require("express");
 const feedRouter = express.Router();
 
-module.exports = feedRouter;
+// Get route for main feed
+feedRouter.get("/", async (req, res) => {
+  const { q, filter = "hot" } = req.query; // Grabs query paramaeters
+  try {
+    const response = q
+      ? // If query is not empty, display queried results with filter applied
+        await fetch(`https://api.reddit.com/search.json?q=${q}&sort=${filter}`)
+      : // If query is empty, show default feed with the hot filter applied
+        await fetch(`https://api.reddit.com/${filter}.json`);
+
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch feed data" });
+  }
+});
+
+//Get route when searching for subreddits
+feedRouter.get("/:subreddit", async (req, res) => {
+  const subreddit = req.params.subreddit;
+  try {
+    const response = await fetch(
+      `https://www.reddit.com/subreddits/search.json?q=${subreddit}`
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: "Could not fetch subreddits" });
+  }
+});
+
+module.exports = { feedRouter };
