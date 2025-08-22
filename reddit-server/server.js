867d573c5234d795b3cf059d7a17ca7c6a828027
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-08-22 12:51:22 -0400

    refactor(server): update router imports and port, add default .get() message

--- reddit-server/server.js
+++ reddit-server/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-import feedRouter from "./routes/feed";
-import postRouter from "./routes/posts";
-import commentsRouter from "./routes/comments";
+const { feedRouter } = require("./routes/feed.js");
+const { postRouter } = require("./routes/posts.js");
+const { commentsRouter } = require("./routes/comments.js");
 app.use(express.json());
 app.use(cors());
 // Route registers
 app.use("/feed", feedRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentsRouter);
+app.get("/", (req, res) => {
+  res.send("Server up and running");
+});
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
