619e3b24cbb31fc01d803181a87f0fcf1ab28c58
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-09-08 13:07:31 -0400

    feat(posts): add route for getting subreddit posts

--- reddit-server/routes/posts.js
+++ reddit-server/routes/posts.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const postRouter = express.Router();
+// Get route for subreddit posts
+postRouter.get("/subreddit/:subredditName/:filter", async (req, res) => {
+  const { subredditName, filter } = req.params;
+  try {
+    const response = await fetch(
+      `https://www.reddit.com/r/${subredditName}/${filter}.json`
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: "Failed to fetch posts" });
+  }
+});
 
-module.exports = postRouter;
+module.exports = { postRouter };
