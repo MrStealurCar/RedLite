cff8562fe4b7c7ae5c363ad0c03f7d4c11b89b6c
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-09-08 12:47:44 -0400

    feat(comments): add route for fetching comments

--- reddit-server/routes/comments.js
+++ reddit-server/routes/comments.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const commentsRouter = express.Router();
 
-module.exports = commentsRouter;
+//Route to fetch comments on posts
+commentsRouter.get("/:comments", async (req, res) => {
+  const comments = req.params.comments;
+  try {
+    const response = await fetch(
+      `https://www.reddit.com/comments/${comments}.json`
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: "Could not fetch comments" });
+  }
+});
+
+module.exports = { commentsRouter };
